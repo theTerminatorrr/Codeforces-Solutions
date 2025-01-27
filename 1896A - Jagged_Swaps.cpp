b4176e274eebe4d8e0bcca7127cd10b47c2ed893
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2025-01-27 21:47:49 +0600

    Add files via upload

---
+++ 1896A - Jagged_Swaps.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+
+        for (int i = 0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+        if(v[0] != 1)
+        {
+            cout << "No\n";
+        }
+        else    cout << "Yes\n";
+
+    }
+}
